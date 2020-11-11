55fb3e9db3bbf469f6a9133344f96caf9a969990
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-11 21:56:50 +0530

    updated

--- app/src/main/java/com/dissanayake/practiceteories/MainActivity.kt
+++ app/src/main/java/com/dissanayake/practiceteories/MainActivity.kt
@@ -2,10 +2,19 @@ package com.dissanayake.practiceteories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.firstFrameLayout, firstFragment)
+            commit()
+        }
     }
 }
