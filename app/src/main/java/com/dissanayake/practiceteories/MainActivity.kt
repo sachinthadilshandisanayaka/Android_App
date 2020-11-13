334cb57e8fa3f27b6196e9ee9e35c17a72a3c835
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-14 01:20:11 +0530

    added action for menu items

--- app/src/main/java/com/dissanayake/practiceteories/MainActivity.kt
+++ app/src/main/java/com/dissanayake/practiceteories/MainActivity.kt
@@ -3,8 +3,11 @@ package com.dissanayake.practiceteories
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,8 +37,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // this method must be public
+    public fun onSearchClick(menuItem: MenuItem) {
+    showToast("Search")
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        
+        menuInflater.inflate(R.menu.menu_one, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.item2 -> {
+                showToast("item 2")
+                return true
+            }
+            R.id.item3 -> {
+                showToast("item 3")
+                return true
+            }
+            R.id.subItem1 -> {
+                showToast("item 4")
+                return true
+            }
+            R.id.subItem2 -> {
+                showToast("item 5")
+                return true
+            }
+            R.id.subItem3 -> {
+                showToast("item 6")
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+    public fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
