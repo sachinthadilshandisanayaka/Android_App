cbbc908360f619ca78950cfd3e5f652a57688cff
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-11-17 01:13:34 +0530

    added drawer navigation

--- app/src/main/java/com/dissanayake/practiceteories/ASecondActivity.kt
+++ app/src/main/java/com/dissanayake/practiceteories/ASecondActivity.kt
@@ -1,11 +1,65 @@
 package com.dissanayake.practiceteories
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_a_second.*
+import java.util.*
 
 class ASecondActivity : AppCompatActivity() {
+
+    // navigation controller
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var listener: NavController.OnDestinationChangedListener
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_a_second)
+
+        //  fragment id
+        navController = findNavController(R.id.fragment)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        // id of the Navigation view in the xml file
+        navigationView.setupWithNavController(navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
+            if(destination.id == R.id.newFirstFragment) {
+                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.design_default_color_primary_dark)))
+            } else if (destination.id == R.id.navSecondFragment) {
+                supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.purple_200)))
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(listener)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navController.removeOnDestinationChangedListener(listener)
+    }
+
+    // maintain "drawer menu" in navigation activity
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
